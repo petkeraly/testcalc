cdee143ccbca58d65f35547b5ea0c2339241b4a3
petkeraly <36895236+petkeraly@users.noreply.github.com>
2018-02-27 19:26:04 +0100

    Updated the calc
    Added the ability to use the four basic operations.
    Added the ability to choose quit.

--- testcalc.py
+++ testcalc.py
@@ -1,5 +1,5 @@
 print("Test calculator.\nThis is an example project made by Peter Dobosi.\n")
-print("Type\n\nfor addition:\n   add or ADD or 1 or +\n\nfor substraction:\n   sub or SUB or 2 or -\n\nfor multiplication\n   mul or MUL or 3 or *\n\nfor division\n   div or DIV or 4 or /\n")
+print("Type\n\nfor addition:\n   add or ADD or 1 or +\n\nfor substraction:\n   sub or SUB or 2 or -\n\nfor multiplication\n   mul or MUL or 3 or *\n\nfor division\n   div or DIV or 4 or /\n\nto quit\n quit or QUIT or 5\n")
 stri=input("What would you like to do?\n")
 if stri=="ADD" or stri=="add" or stri=="1" or stri=="+":
       num1=float(input("Please enter the first operand: "))
@@ -11,13 +11,39 @@ if stri=="ADD" or stri=="add" or stri=="1" or stri=="+":
             numsum=numsum+szam
             booi=input("Would you like to use another operand?\n")
       print (numsum)
-if stri=="SUB" or stri=="sub" or stri=="2" or stri=="-":
+elif stri=="SUB" or stri=="sub" or stri=="2" or stri=="-":
       num1=float(input("Please enter the first operand: "))
       num2=float(input("Please enter the second operand: "))
-      booi=input("Would you like to use more operands?\n Type true or false: ")
+      booi=input("Would you like to use more operands?\n Type yes or no\n")
       numsum=float(num1-num2)
       while booi=="true":
             szam=float(input("What\'s your operand?\n"))
             numsum=numsum-szam
             booi=input("Would you like to use another operand?\n")
       print (numsum)
+elif stri=="MUL" or stri=="mul" or stri=="3" or stri=="*":
+      num1=float(input("Please enter the first operand: "))
+      num2=float(input("Please enter the second operand: "))
+      booi=input("Would you like to use more operands?\n Type yes or no\n")
+      numsum=float(num1*num2)
+      while booi=="true":
+            szam=float(input("What\'s your operand?\n"))
+            numsum=numsum*szam
+            booi=input("Would you like to use another operand?\n")
+      print (numsum)
+elif stri=="DIV" or stri=="div" or stri=="4" or stri=="/":
+      num1=float(input("Please enter the first operand: "))
+      num2=float(input("Please enter the second operand: "))
+      booi=input("Would you like to use more operands?\n Type yes or no\n")
+      numsum=float(num1/num2)
+      while booi=="true":
+            szam=float(input("What\'s your operand?\n"))
+            numsum=numsum/szam
+            booi=input("Would you like to use another operand?\n")
+      print (numsum)
+elif stri=="quit" or stri=="QUIT" or stri=="5":
+      print("You have selected the option to quit.\nGoodbye, have a nice day!")
+else:
+      print("Unknown input")
+print("Program finished.\nQuitting...")
+
